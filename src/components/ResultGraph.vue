42c262c123ea59c1ea6205cd2d3f8251eebcc6e6
Nikita Rastorguev <nirastor@gmail.com>
2022-05-01 16:32:36 +0300

    Добавил chart-js и данные для графика — без тонокой настройки

--- src/components/ResultGraph.vue
+++ src/components/ResultGraph.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    <div>{{ $store.getters.getFakeDataForGraph }}</div>
-  </div>
+  <Pie :chart-data="chartData" :chart-options="chartOptions" />
 </template>
+
+<script>
+import { Pie } from 'vue-chartjs/legacy'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
+
+export default {
+  name: 'BarChart',
+  components: { Pie },
+  data() {
+    return {
+      chartOptions: {},
+    }
+  },
+  computed: {
+    chartData() {
+      const rawData = this.$store.getters.getFakeDataForGraph
+      console.log(this.$store.getters.getFakeDataForGraph)
+      return {
+        labels: Object.keys(rawData),
+        datasets: [
+          {
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+            data: Object.values(rawData),
+          },
+        ],
+      }
+    },
+  },
+}
+</script>
